2302a84d6f044d9d4f07de6e2e87f636971df4f9
Yuki Kaito <ykaito21@gmail.com>
2018-05-21 14:03:33 +0900

    testing with capybara gem

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  # Adds support for Capybara system testing and selenium driver
+  gem 'capybara', '~> 2.13'
+  gem 'selenium-webdriver'
 end
 
 group :development do
@@ -52,8 +55,6 @@ end
 
 group :test do
   # Adds support for Capybara system testing and selenium driver
-  gem 'capybara', '>= 2.15', '< 4.0'
-  gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
 end
---
+++ test/system/questions_test.rb
@@ -0,0 +1,30 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit questions_url
+  #
+  #   assert_selector "h1", text: "Questions"
+  # end
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "saying with ? yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "What time is it?"
+    click_on "Ask"
+
+    assert_text "Silly question, get dressed and go to work!"
+    take_screenshot
+  end
+end
